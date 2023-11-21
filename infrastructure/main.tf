0f0db7c2164f4787d1d3273003c9ae5e8cce2fc2
Gökhan Karasakal <gokhankarasakal@gkarasakals-MacBook-Pro.local>
2023-11-21 22:56:39 +0100

    Adding variables in json format

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,21 +1,25 @@
+locals {
+  terraform_variables = jsondecode(file("${path.module}/variables.json"))
+}
+
 module "vpc" {
   source              = "../modules/vpc"
-  env                 = local.env
-  private_cidr_blocks = var.private_cidr_blocks[local.env]
-  public_cidr_blocks  = var.public_cidr_blocks[local.env]
-  vpc_cidr_block      = var.vpc_cidr_block[local.env]
+  vpc_cidr_block      = local.terraform_variables.variable.vpc_cidr_blocks.default
+  env                 = local.terraform_variables.locals.env
+  private_cidr_blocks = local.terraform_variables.variable.private_cidr_blocks.default[*]
+  public_cidr_blocks  = local.terraform_variables.variable.public_cidr_blocks.default[*]
 }
 
 module "loadbalancer" {
   source              = "../modules/loadbalancer"
-  subnets             = local.public_subnet_ids
-  vpc_id              = local.vpc_id
-  web_traffic         = local.web_traffic[local.env]
+  subnets             = module.vpc.public_subnet.*.id
+  vpc_id              = module.vpc.vpc_id
+  web_traffic         = local.terraform_variables.variable.web_traffic.default
 }
 
 module "autoscaling" {
   source              = "../modules/autoscaling"
-  vpc_id              = local.vpc_id
+  vpc_id              = module.vpc.vpc_id
   alb_sg              = module.loadbalancer.dpg_amv_alb_sg_id
   private_subnet_id   = module.vpc.private_subnet[0].id
   public_subnet_id    = module.vpc.public_subnet[0].id
